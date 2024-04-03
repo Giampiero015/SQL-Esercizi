3b13a50889943318bb93c14e701e4972522ca19a
Giampiero Franconieri <150815405+Giampiero015@users.noreply.github.com>
2024-04-03 11:34:04 +0200

    Update esercizio1.sql

--- esercizio1.sql
+++ esercizio1.sql
@@ -1,1 +1,11 @@
-prova
+CREATE TABLE Books (
+  book_id INTEGER PRIMARY KEY,
+  title VARCHAR(20),
+  author VARCHAR(100),
+  genere VARCHAR(100),
+  published_year DATE,
+  isbn VARCHAR(20),
+  price DECIMAL(3,2),
+  rating DECIMAL(2,1),
+  stock_count INTEGER
+);
